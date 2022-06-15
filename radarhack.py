e1473b17b07cc6fd8b17b9fc9c59419db5d86eea
Jesewe <81848878+Jesewe@users.noreply.github.com>
2022-06-15 18:45:02 +0600

    Update radarhack.py

--- radarhack.py
+++ radarhack.py
@@ -6,8 +6,8 @@ client = pymem.process.module_from_name(pm.process_handle,
                                         'client.dll')
 
 clientModule = pm.read_bytes(client.lpBaseOfDll, client.SizeOfImage)
-address = client.lpBaseOfDll + re.search(rb'\x83\xE0\x0F\x80\xBF',
-                                         clientModule).start() + 9
+address = client.lpBaseOfDll + re.search(rb'\x74\x15\x8B\x47\x08\x8D\x4F\x08',
+                                         clientModule).start() - 1
 
 pm.write_uchar(address, 0 if pm.read_uchar(address) != 0 else 2)
 pm.close_process()
